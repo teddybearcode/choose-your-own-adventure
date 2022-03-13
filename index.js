77464d8994cf7e71d0c02f23e3130a67d6633786
Leo Trius <leotrius@gmail.com>
2022-03-13 14:35:30 -0500

    coding notes & functions

---
+++ index.js
@@ -0,0 +1,14 @@
+// let ansers = prompt("soooo, this is my website?!!?");
+// alert("soooo," + " HELP ME MY SANITY IS DISsIPIRING!!!!");
+// a function is a pice of code that you can run, running a function
+// is called calling a function, a function can recive data using paramitars
+// and it can return data. a function call looks like this:
+// returnValue = function(parameter1, parameter2)
+// for example: alert("hi")
+
+function sayHi() {
+    alert("hi");
+}
+function sayhello() {
+    alert("hello");
+}
