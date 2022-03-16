5a11be3634a60885beffe5d54302845979ba16d3
Leo Trius <leotrius@gmail.com>
2022-03-15 20:23:17 -0500

    added buttons

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-function sayHi() {
-    alert("hi");
+
+function button1() {
+    console.log("button1");
+
+    let scene = document.querySelector('#js-scene');
+    scene.innerHTML = "<p>you clicked Button 1</p>";
 }
-function sayHello() {
-   alert("Hello");
+function button2() {
+   console.log("button2");
+
+   let scene = document.querySelector('#js-scene');
+   scene.innerHTML = "<p>you clicked Button 2</p>"
+}
+function button3(){
+    console.log("button3");
+
+    let scene = document.querySelector('#js-scene');
+    scene.innerHTML = "<p>you clicked button 3</p>";
+}
+function button4(){
+    console.log("button4");
+
+    let scene = document.querySelector('#js-scene');
+    scene.innerHTML = "<p>you clicked button4</p>";
 }
