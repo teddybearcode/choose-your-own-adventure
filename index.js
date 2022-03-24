271189d5af90fe2a6ea329dcf52e34d34a3f996d
Leo Trius <leotrius@gmail.com>
2022-03-23 19:34:34 -0500

    new varible

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-
+let lastButtonClicked;
 function button1() {
     console.log("button1");
 
     let scene = document.querySelector('#js-scene');
     scene.innerHTML = "<p>you clicked Button 1</p>";
+    lastButtonClicked = "1";
 }
 function button2() {
    console.log("button2");
@@ -15,11 +16,11 @@ function button3(){
     console.log("button3");
 
     let scene = document.querySelector('#js-scene');
-    scene.innerHTML = "<p>you clicked button 3</p>";
+    scene.innerHTML = "<p>you clicked Button 3</p>";
 }
 function button4(){
     console.log("button4");
 
     let scene = document.querySelector('#js-scene');
-    scene.innerHTML = "<p>you clicked button4</p>";
+    scene.innerHTML = "<p>you clicked Button 4</p>";
 }
