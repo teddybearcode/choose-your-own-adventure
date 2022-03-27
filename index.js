e0a033b3390deee87ad8666f9c710317ebd40a95
Leo Trius <leotrius@gmail.com>
2022-03-27 11:22:47 -0500

    NEW BUTTON STUFF!!!!

--- index.js
+++ index.js
@@ -4,25 +4,33 @@ function button1() {
 
     let scene = document.querySelector('#js-scene');
     scene.innerHTML = "<p>you clicked Button 1</p>";
-    
-    
-    lastButtonClicked = "1";
-
-    
 }
 function button2() {
-   console.log("button2");
+    console.log("button2");
 
-   let scene = document.querySelector('#js-scene');
-   scene.innerHTML = "<p>you clicked Button 2</p>"
+    // let scene = document.querySelector('#js-scene');
+    //scene.innerHTML = "<p>you clicked Button 2</p>"
+
+
+    let answer1 = prompt("how are you?");
+    if (answer1 == 'good') {
+        let scene = document.querySelector('#js-scene');
+        scene.innerHTML = "<p>amazing</p>"
+    }
+
+    if (answer1 == 'bad') {
+        let scene = document.querySelector('#js-scene');
+        scene.innerHTML = "<p>oh no!!!</p>"
+
+    }
 }
-function button3(){
+function button3() {
     console.log("button3");
 
     let scene = document.querySelector('#js-scene');
     scene.innerHTML = "<p>you clicked Button 3</p>";
 }
-function button4(){
+function button4() {
     console.log("button4");
 
     let scene = document.querySelector('#js-scene');
